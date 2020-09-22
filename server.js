faa2a9f573bdca0a960acb974c1822ab57e6ced0
CaptainATG <apigoarjaynnyl.aa@gmail.com>
2020-09-22 15:43:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ const app = express();
 
 /* Frontend */
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "../frontend/views"));
+app.set("views", path.join(__dirname, "frontend/views"));
 /* Serve Static Files */
-app.use("/static", express.static(path.join(__dirname, "../frontend/static")));
+app.use("/static", express.static(path.join(__dirname, "frontend/static")));
 
 /* Middleware */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 /* Routes */
-const appRoute = require("./routes/app");
+const appRoute = require("./backend/routes/app");
 
 app.use("/", appRoute);
 
 const PORT = 3001;
 
 app.listen(PORT, () => {
-  console.log(`POrt ni Arjay: ${PORT}`);
+  console.log(`Port ni Arjay: ${PORT}`);
 });
